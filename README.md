8c60e2a484b1d04b21fee1935d12fbc8d296c0ff
namniak <vanmnyak@gmail.com>
2014-06-20 05:01:21 -0400

    v0.1

--- README.md
+++ README.md
@@ -3,21 +3,52 @@ imgCoverEffect
 
 A JavaScript framework-independent solution for simulating CSS 'background-size: cover' effect on HTMLImageElement.
 
-Optionally, horizontal and vertical alignments can be set by passing an object *{x: value1, y: value2}* as the second parameter with the following available values:
+##Syntax
+```
+imgCoverEffect(HTMLImageElement [, options]);
+```
+where options is an objects with the following available properties and values:
+```
+- alignX: 'left' (default) || 'center' || 'right'   // horizontal align
+    
+- alignY: 'top'  (default) || 'middle' || 'bottom'  // vertical align
+    
+- listenToLoad: true (default) || false             // load event listener
+    
+- watchResize: true (default) || false              // 
+```
 
-- x: 'left', 'center', 'right'
-- y: 'top', 'middle', 'bottom'
+If no options object is passed, default values will apply.
 
-If no optional object is passed, the image will be aligned the top and left by default.
-
-####Version 0.1
+####Version: 0.1
 
 ==============
 
-##Usage
+##Usage:
 ```
-var image = document.createElement('img');
-imgCoverEffect(image, {x: 'center', y: 'middle'});
+1. With a newly created Image element
+
+var img = new Image();
+img.src = 'pathToImg';
+document.body.appendChild(img);
+
+imgCoverEffect(img, {
+  alignX: 'center',
+  alignY: 'middle'
+});
+
+The above code will automatically listen to 'load' event and adapt image on DOM resize
+
+2. With existing image
+
+img = document.getElementById('imgID');
+imgCoverEffect(img, {
+  alignX: 'center',
+  alignY: 'middle'
+  listenToLoad: false,  // suppose the function is called on 'load' event explicitly
+  watchResize: true
+});
+
 ```
 
 ##Install
--- imgCoverEffect.js
+++ imgCoverEffect.js
@@ -1,79 +1,149 @@
-/*! imgCoverEffect
+/*! imgCoverEffect (https://github.com/namniak/imgCoverEffect)
+ *  Version:  0.1
  *
- *  @license: n/a, free
- *  @version: 0.1
- *  @author   Vadim Namniak
- *  @contact  vnamnyak@gmail.com
- *  @github:  https://github.com/namniak/imgCoverEffect/edit/master/imgCoverEffect.js
+ *  MIT License (http://www.opensource.org/licenses/mit-license.html)
+ *  Copyright (c) 2014 Vadim Namniak
  */
 
-var imgCoverEffect = function(image, align) {
+function imgCoverEffect(image, opts) {
     'use strict';
 
     if (!(image instanceof HTMLImageElement)) {
         throw new Error('From imgCoverEffect(): Element passed as a parameter is not an instance of HTMLImageElement.');
     }
 
-    var aspectRatio = image.naturalWidth / image.naturalHeight;
-    var screenW = window.innerWidth;
-    var screenH = window.innerHeight;
+    if (opts.listenToLoad && typeof opts.listenToLoad !== 'boolean') {
+        throw new Error('From imgCoverEffect(): "listenToLoad" property must be set to a Boolean if the option is specified.');
+    }
+
+    if (opts.watchResize && typeof opts.watchResize !== 'boolean') {
+        throw new Error('From imgCoverEffect(): "watchResize" property must be set to a Boolean if the option is specified.');
+    }
+
+    var parent = image.parentNode;
+    var lastParentWidth = null;
+    var lastParentHeight = null;
+    var currParentWidth = null;
+    var currParentHeight = null;
+    var aspectRatio = null;
 
     // set default styles
+    parent.style.overflow = 'hidden';
+    parent.style.position = 'relative'; // to apply overflow with absolutely positioned image child element, can be also set to 'absolute' or 'fixed' if needed
     image.style.position = 'absolute';
     image.style.top = 0;
     image.style.left = 0;
-    image.style.margin = 0;
+    image.style.zIndex = -1;
 
-    if (screenW >= screenH) {
-        // set image sizes for landscape screen orientation
-        image.width = screenW;
-        image.height = image.width / aspectRatio;
-
-        // check how the new image height fits the window
-        if (image.height < screenH) {
-            // adjust the sizes accordingly when the current image height
-            // is too small to cover the screen
-            image.height = screenH;
-            image.width = image.height * aspectRatio;
-        }
+    // set events
+    if (opts.listenToLoad === false) {
+        resizeImg();
     } else {
-        // set image sizes for portrait screen orientation
-        image.height = screenH;
-        image.width = image.height * aspectRatio;
-
-        // check how the new image width fits the window
-        if (image.width < screenW) {
-            // adjust the sizes accordingly when the current image width
-            // is too small to cover the screen
-            image.width = screenW;
-            image.height = image.width / aspectRatio;
+        if (image.addEventListener) {
+            image.addEventListener('load', resizeImg, false);
+        } else if (image.attachEvent) {
+            image.attachEvent('onload', resizeImg);
         }
     }
 
-    if (align) {
-        // horizontal align
-        if (align.x === 'left' || !align.x) {
-            image.style.left = 0;
-        } else if (align.x === 'center') {
-            image.style.left = (screenW - image.width) / 2 + 'px';
-        } else if (align.x === 'right') {
-            image.style.left = screenW - image.width + 'px';
-        } else {
-            throw new Error('From imgCoverEffect(): Unknown horizontal align value is used. ' +
-                'Property "x" can only be set to one of the following values: "left", "center", or "right".');
+    function resizeImg() {
+        // set DOM resize watcher for parent element
+        if (opts.watchResize !== false) {
+            requestAnimationFrame(resizeImg);
         }
 
-        // vertical align
-        if (align.y === 'top' || !align.y) {
-            image.style.top = 0;
-        } else if (align.y === 'middle') {
-            image.style.top = (screenH - image.height) / 2 + 'px';
-        } else if (align.y === 'bottom') {
-            image.style.top = screenH - image.height + 'px';
-        } else {
-            throw new Error('From imgCoverEffect(): Unknown vertical align value is used. ' +
-                'Property "y" can only be set to one of the following values: "top", "middle", or "bottom".');
+        if (!currParentWidth && !currParentHeight) {
+            // needs to fire once after the image is loaded
+            aspectRatio = image.naturalWidth / image.naturalHeight;
+        }
+
+        currParentWidth = parent.clientWidth;
+        currParentHeight = parent.clientHeight;
+
+        // check if parent was resized and the image needs to adjust
+        if (currParentWidth !== lastParentWidth || currParentHeight !== lastParentHeight) {
+
+            if (currParentWidth >= currParentHeight) {
+                image.width = currParentWidth;
+                image.height = image.width / aspectRatio;
+
+                // check how the new image height fits the parent element
+                if (image.height < currParentHeight) {
+                    // adjust sizes accordingly when the current image height is too small to cover its parent
+                    image.height = currParentHeight;
+                    image.width = image.height * aspectRatio;
+                }
+            } else {
+                image.height = currParentHeight;
+                image.width = image.height * aspectRatio;
+
+                // check how the new image width fits the parent element
+                if (image.width < currParentWidth) {
+                    // adjust sizes accordingly when the current image width is too small to cover its parent
+                    image.width = currParentWidth;
+                    image.height = image.width / aspectRatio;
+                }
+            }
+
+            lastParentWidth = currParentWidth;
+            lastParentHeight = currParentHeight;
+
+            // set horizontal align
+            if (opts.alignX === 'left' || !opts.alignX) {
+                image.style.left = 0;
+            } else if (opts.alignX === 'center') {
+                image.style.left = (currParentWidth - image.width) / 2 + 'px';
+            } else if (opts.alignX === 'right') {
+                image.style.left = currParentWidth - image.width + 'px';
+            } else {
+                throw new Error('From imgCoverEffect(): Unsupported horizontal align value is used. ' +
+                    'Property "alignX" can only be set to one of the following values: "left", "center", or "right".');
+            }
+
+            // set vertical align
+            if (opts.alignY === 'top' || !opts.alignY) {
+                image.style.top = 0;
+            } else if (opts.alignY === 'middle') {
+                image.style.top = (currParentHeight - image.height) / 2 + 'px';
+            } else if (opts.alignY === 'bottom') {
+                image.style.top = currParentHeight - image.height + 'px';
+            } else {
+                throw new Error('From imgCoverEffect(): Unsupported vertical align value is used. ' +
+                    'Property "alignY" can only be set to one of the following values: "top", "middle", or "bottom".');
+            }
+
         }
     }
+}
 
-};
+/*! requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+ https://gist.github.com/paulirish/1579671
+ MIT license
+ */
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+            || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() {
+                    callback(currTime + timeToCall);
+                },
+                timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
